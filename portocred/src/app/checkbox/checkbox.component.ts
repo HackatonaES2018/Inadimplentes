08a25fcf5413b8e65ff28d478e4c22960c64a89c
gsbrunichaki <gabriel.brunichaki@acad.pucrs.br>
2018-11-18 06:42:44 -0200

    fatura screen

--- portocred/src/app/checkbox/checkbox.component.ts
+++ portocred/src/app/checkbox/checkbox.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api-service.service'
+import { ApiService } from '../api-service.service';
 import { Parcela, Contrato, Cliente} from '../app.interfaces';
 
 @Component({
@@ -8,38 +8,32 @@ import { Parcela, Contrato, Cliente} from '../app.interfaces';
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent implements OnInit {
-  parcela : Parcela;
+  parcela: Parcela;
   rendaParcela1: number;
   rendaParcela2: number;
-  
 
   constructor(
     private apiService: ApiService
-  ){}
+  ) {
+  }
 
   async ngOnInit(){
-    
-    let temp: any = await this.apiService.getpropostaById(1);
+    const temp: any = await this.apiService.getpropostaById(1);
     console.log('Resposta do Get Proposal');
     console.log(temp);
 
-    let test: Cliente = temp.cliente
+    const test: Cliente = temp.cliente;
     console.log(test);
-    
-    let contrato: any = await this.apiService.getcontratosByCpf(test.cpf);
+    const contrato: any = await this.apiService.getcontratosByCpf(test.cpf);
     await this.apiService.messageWpp();
 
-    let toParcela1:Parcela = contrato.parcela[0];
-    let toParcela2:Parcela = contrato.parcela[1];
-    
-    this.rendaParcela1 = toParcela1.renda
-    this.rendaParcela2 = toParcela2.renda
+    const toParcela1: Parcela = contrato.parcela[0];
+    const toParcela2: Parcela = contrato.parcela[1];
+    this.rendaParcela1 = toParcela1.renda;
+    this.rendaParcela2 = toParcela2.renda;
 
-    console.log('Teste Aqui')
-    console.log(this.rendaParcela1)
-    
-    
-    
+    console.log('Teste Aqui');
+    console.log(this.rendaParcela1);
   }
 
 }

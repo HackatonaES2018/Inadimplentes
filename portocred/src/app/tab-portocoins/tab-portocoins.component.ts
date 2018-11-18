8048089768a562682a806f5ec2b84e58bdd8523a
Marcelo <marcelobernardy@gmail.com>
2018-11-18 06:41:25 -0200

    tsts

--- portocred/src/app/tab-portocoins/tab-portocoins.component.ts
+++ portocred/src/app/tab-portocoins/tab-portocoins.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api-service.service'
+import { Cliente, Contrato} from '../app.interfaces';
 
 @Component({
   selector: 'app-tab-portocoins',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab-portocoins.component.css']
 })
 export class TabPortocoinsComponent implements OnInit {
+  nomeCliente: string;
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ){}
 
-  ngOnInit() {
+  async ngOnInit(){
+    
+    let temp: any = await this.apiService.getpropostaById(1);
+    console.log('Resposta do Get Proposal');
+    console.log(temp);
+
+    let test: Cliente = temp.cliente
+    console.log(test);
+    
+    let contrato:Contrato = await this.apiService.getcontratosByCpf(test.cpf);
+    await this.apiService.messageWpp();
+
+    console.log('Teste Aqui')
+    console.log(test.nome)
+    this.nomeCliente = test.nome;
+    
   }
 
 }

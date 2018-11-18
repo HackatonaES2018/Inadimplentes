83e2079ed79781b866c39907b978746c65068f77
Marcelo <marcelobernardy@gmail.com>
2018-11-18 03:10:11 -0200

    api wpp

--- portocred/src/app/app.component.ts
+++ portocred/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 import { ApiService } from './api-service.service'
-import { Resposta } from 
+import { Cliente, Contrato } from './app.interfaces' 
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,20 @@ export class AppComponent implements OnInit{
     private apiService: ApiService
   ){}
 
-  ngOnInit(){
-    let test  = this.apiService.getpropostaById(1);
+  async ngOnInit(){
+    
+    let temp: any = await this.apiService.getpropostaById(1);
+    console.log('Resposta do Get Proposal');
+    console.log(temp);
+
+    let test: Cliente = temp
+    console.log(test);
+    
+    let contrato:Contrato = await this.apiService.getcontratosByCpf(test.cpf);
+    await this.apiService.messageWpp();
 
     console.log('Teste Aqui')
-    console.log(test)
+    console.log(test.nome)
     
   }
 }

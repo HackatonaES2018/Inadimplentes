8f78e124a0a8bde671010979fda6cf6700366226
Marcelo <marcelobernardy@gmail.com>
2018-11-18 06:20:46 -0200

    component ts logic array

--- portocred/src/app/checkbox/checkbox.component.ts
+++ portocred/src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api-service.service'
+import { Parcela, Contrato, Cliente} from '../app.interfaces';
 
 @Component({
   selector: 'app-checkbox',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent implements OnInit {
+  parcela : Parcela;
+  rendaParcela1: number;
+  rendaParcela2: number;
+  
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ){}
 
-  ngOnInit() {
+  async ngOnInit(){
+    
+    let temp: any = await this.apiService.getpropostaById(1);
+    console.log('Resposta do Get Proposal');
+    console.log(temp);
+
+    let test: Cliente = temp.cliente
+    console.log(test);
+    
+    let contrato: any = await this.apiService.getcontratosByCpf(test.cpf);
+    await this.apiService.messageWpp();
+
+    let toParcela1:Parcela = contrato.parcela[0];
+    let toParcela2:Parcela = contrato.parcela[1];
+    
+    this.rendaParcela1 = toParcela1.renda
+    this.rendaParcela2 = toParcela2.renda
+
+    console.log('Teste Aqui')
+    console.log(this.rendaParcela1)
+    
+    
+    
   }
 
 }
